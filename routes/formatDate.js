b22d9b25e8f75fba82f90ccec9412310a64f09ab
Gaox <15511692608@163.com>
2017-05-04 22:27:32 +0800

    json

--- routes/formatDate.js
+++ routes/formatDate.js
@@ -5,9 +5,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-
+router.get('/', function(req, res) {
+	console.log(req.body);
+	
     res.render('index', { title: 'formatDate',date:req.body  });
 });
 
+/* POST home page. */
+router.post('/', function(req, res) {
+    console.log(req.body);
+
+    res.send( { title: 'formatDate',date:req.body  });
+});
 module.exports = router;
