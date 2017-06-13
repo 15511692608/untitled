eb6a47ca2d313a9efe98796ded4676d09a931946
15511692608 <15511692608@163.com>
2017-06-13 16:29:50 +0800

    ts

---
+++ trace.config.js
@@ -0,0 +1,8 @@
+/**
+ * Created by admin on 2017/6/13.
+ */
+// trace.config.js
+module.exports = {
+    serviceName: 'test',
+    apiKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjU5M2Y4NmJiMzBlMjJmMmQyOWIyZDhhNSIsImlhdCI6MTQ5NzMzNTQ4M30.yuZOR2TUkLWimbtaR0iAHcwfOCcFAZkOpWGA6Mb10C0'
+};
